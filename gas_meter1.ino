8b9b108f6122895b8789147f1835bf600a0873a6
xriser <spm@z1.kiev.ua>
2017-05-13 15:00:26 +0300

    fix errors

--- gas_meter1.ino
+++ gas_meter1.ino
@@ -1,6 +1,6 @@
 extern "C" {
-  #include "user_interface.h"
-} 
+#include "user_interface.h"
+}
 
 // Include the libraries we need
 #include <OneWire.h>
@@ -17,15 +17,14 @@ const char *DB_PASSWORD = "";
 
 #define ONE_WIRE_BUS 4 // Data wire is plugged into port 2 on the Arduino
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
 DeviceAddress insideThermometer =  {0x28, 0xFA, 0xAB, 0x8C, 0x06, 0x00, 0x00, 0xFA};
-DeviceAddress outsideThermometer = {0x28, 0x17, 0x1C, 0x8C, 0x06, 0x00, 0x00, 0x87};
 
-int Led=15;// Definition LED Interface
-int SENSOR=2;// Define the Hall magnetic sensor interface
+int Led = 15; // Definition LED Interface
+int SENSOR = 2; // Define the Hall magnetic sensor interface
 int val;// Defines a numeric variable val
-int hall_state=0;
-int count=0;
+int hall_state = 0;
+int count = 0;
 
 #define GLED      13
 #define GLED_ON   digitalWrite(GLED, 1)
@@ -37,14 +36,14 @@ WiFiServer server(80);
 
 void setup() {
   pinMode(GLED, OUTPUT);
-  pinMode(Led,OUTPUT);// Definition LED as output interface
-  pinMode(SENSOR,INPUT);// Defined the Hall magnetic sensor as output interface
-  sensors.setResolution(insideThermometer, 11); 
-  
+  pinMode(Led, OUTPUT); // Definition LED as output interface
+  pinMode(SENSOR, INPUT); // Defined the Hall magnetic sensor as output interface
+  sensors.setResolution(insideThermometer, 11);
+
   // start serial port
   Serial.begin(115200);
 
-   
+
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
@@ -55,23 +54,21 @@ void setup() {
   Serial.println(" connected");
   Serial.println(WiFi.localIP());
 
- // Start the server
+  // Start the server
   server.begin();
   Serial.println("Server started");
 
   // Print the IP address
   Serial.println(WiFi.localIP());
-  
- influxdb.opendb(DATABASE);
- Serial.println(influxdb.response());
+
 }
 
 void loop() {
- count++;
+  count++;
 
- if (count>=2) 
- {
-  GLED_OFF;  
+  if (count >= 2)
+  {
+    GLED_OFF;
   }
 
   sensors.requestTemperatures(); // Send the command to get temperatures
@@ -82,59 +79,58 @@ void loop() {
   Serial.println(tempC);
 
 
-val=digitalRead(SENSOR); // The digital interface is assigned a value of 3 to read val
+  val = digitalRead(SENSOR); // The digital interface is assigned a value of 3 to read val
 
-if((val==LOW) and (hall_state==0)) // When the Hall sensor detects a signal , flash led and send data
+  if ((val == LOW) and (hall_state == 0)) // When the Hall sensor detects a signal , flash led and send data
   {
-  hall_state=1;  
-  digitalWrite(Led, HIGH);
-  Serial.println("Detect state change to LOW");
-  
-  //send data here
-   
-   HTTPClient http;    //Declare object of class HTTPClient
- 
-   http.begin("http://" + INFLUXDB_HOST + ":" + INFLUXDB_PORT "/write?db=" + DATABASE);      //Specify request destination
-   http.addHeader("Content-Type", "text/plain");  //Specify content-type header
- 
-   int httpCode = http.POST("gas,host=home,region=ua value=1");   //Send the request
-   String payload = http.getString();                  //Get the response payload
- 
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection
- 
-} 
+    hall_state = 1;
+    digitalWrite(Led, HIGH);
+    Serial.println("Detect state change to LOW");
 
-if((val==HIGH) and (hall_state==1)) //When Hall sensor release signal
-  
+    //send data here
+
+    HTTPClient http;    //Declare object of class HTTPClient
+
+    http.begin("http://" + String(INFLUXDB_HOST) + ":" + String(INFLUXDB_PORT) + "/write?db=" + String(DATABASE));      //Specify request destination
+    http.addHeader("Content-Type", "text/plain");  //Specify content-type header
+
+    int httpCode = http.POST("gas,host=home,region=ua value=1");   //Send the request
+    String payload = http.getString();                  //Get the response payload
+
+    Serial.println(httpCode);   //Print HTTP return code
+    Serial.println(payload);    //Print request response payload
+
+    http.end();  //Close connection
+
+  }
+
+  if ((val == HIGH) and (hall_state == 1)) //When Hall sensor release signal
   {
-  digitalWrite(Led, LOW);
-  hall_state=0;
+    digitalWrite(Led, LOW);
+    hall_state = 0;
   }
 
 
- WiFiClient client = server.available(); 
- String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n ";
+  WiFiClient client = server.available();
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n ";
   s += "temp: ";
   s += tempC;
   s += "C</html>\n";
 
   // Send the response to the client
   client.print(s);
-  Serial.println(); 
+  Serial.println();
 
 
-Serial.println(count); 
+  Serial.println(count);
 
-if (count>10) 
- {
-  GLED_ON;  
-  Serial.println("GLED ON"); 
-  count=0;
+  if (count > 10)
+  {
+    GLED_ON;
+    Serial.println("GLED ON");
+    count = 0;
   }
-  
-delay(1000);
+
+  delay(1000);
 }
 
